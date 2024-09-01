5597053d28b4028f81a65ee2cc8af8062a39ef6d
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2024-09-01 01:52:39 -0400

    feat: update theme to MaterialComponents and add translucent system bars
    This commit updates the app theme to use MaterialComponents and adds translucent navigation and status bars for a
     more modern look.

--- android/app/src/main/kotlin/com/turtlepaw/fitness_challenges/MainActivity.kt
+++ android/app/src/main/kotlin/com/turtlepaw/fitness_challenges/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.turtlepaw.fitness_challenges
 
+import android.os.Bundle
+import android.view.View
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity : FlutterFragmentActivity()
