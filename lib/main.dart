4b46f9a78dbd7446f516b8435a58c49732820dac
Turtlepaw <81275769+Turtlepaw@users.noreply.github.com>
2024-09-20 12:20:38 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -159,7 +159,7 @@ void callbackDispatcher() {
       FlutterLocalNotificationsPlugin();
 // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
       const AndroidInitializationSettings initializationSettingsAndroid =
-      AndroidInitializationSettings('ic_notif', useBridgeTags: true);
+      AndroidInitializationSettings('ic_notif');
       final InitializationSettings initializationSettings = InitializationSettings(
           android: initializationSettingsAndroid
       );
